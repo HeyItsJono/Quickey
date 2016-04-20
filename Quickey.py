9e91fd3cf729c2e24ac10ba70e2a61e9ba8bb405
Jono <HeyItsJono@users.noreply.github.com>
2016-04-20 19:45:29 +1000

    Shorten script even more.

--- Quickey.py
+++ Quickey.py
@@ -1,7 +1,5 @@
 import os
 
 args = raw_input("Paste NFSHost clipboard data here (Right-click to paste).\n")
-tid = args.split()[0]
-tkey = args.split()[1]
 
-os.system("FunKeyCIA.py -titleid {0} -key {1}".format(tid, tkey))
+os.system("FunKeyCIA.py -titleid {0} -key {1}".format(args.split()[0], args.split()[1]))
