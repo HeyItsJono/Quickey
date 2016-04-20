46c524cd02d1d4ade449f1f7b08c93c4f43cc870
Jono <HeyItsJono@users.noreply.github.com>
2016-04-20 19:55:02 +1000

    Make compatible with latest FunKeyCIA

--- Quickey.py
+++ Quickey.py
@@ -2,4 +2,4 @@ import os
 
 args = raw_input("Paste NFSHost clipboard data here (Right-click to paste).\n")
 
-os.system("FunKeyCIA.py -titleid {0} -key {1}".format(args.split()[0], args.split()[1]))
+os.system("FunKeyCIA.py -title {0} -key {1}".format(args.split()[0], args.split()[1]))
